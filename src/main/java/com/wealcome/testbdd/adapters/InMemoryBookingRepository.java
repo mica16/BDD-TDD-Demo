5eb23c02679bbfcef5f412dfe0f3f6a2f8ee644f
Michael Azerhad <m.azerhad@wealcome.com>
2017-10-17 02:58:05 +0200

    remove useless spaces

--- src/main/java/com/wealcome/testbdd/adapters/InMemoryBookingRepository.java
+++ src/main/java/com/wealcome/testbdd/adapters/InMemoryBookingRepository.java
@@ -14,6 +14,4 @@ public class InMemoryBookingRepository implements BookingRepository {
         return bookings;
     }
 
-
-
 }
--- src/main/java/com/wealcome/testbdd/usecases/BookVTC.java
+++ src/main/java/com/wealcome/testbdd/usecases/BookVTC.java
@@ -4,7 +4,6 @@ import com.wealcome.testbdd.domain.VTC;
 
 public class BookVTC {
 
-
     public void handle(VTC vtc, String startPoint, String destinationPoint) {
 
     }
